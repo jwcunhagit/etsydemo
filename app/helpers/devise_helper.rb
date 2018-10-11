a0c9ce84ee84c1b3ba6d79817f644b3e7dac09eb
Jefferson Cunha <jwcunha@gmail.com>
2018-10-11 17:55:28 -0300

    Attempt to fix the devise error but withh no success but some improvements

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,2 +1,24 @@
 module DeviseHelper
+	def devise_error_messages!
+		return '' if resource.errors.empty?
+
+		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+		sentence = I18n.t('errors.messages.not_saved', count: resource.errors.count, resource: resource.class.model_name.human.downcase)
+
+		flash.each do |messages, message| 
+			
+
+		end
+
+		html = <<-HTML 
+			<div class="alert">
+				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times</button>
+				<h4>#{sentence}</h4>
+				#{messages}
+			</div>
+        HTML
+
+        html.html_safe
+    end
 end
+
