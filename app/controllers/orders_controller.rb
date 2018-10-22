c3c10c163a67baa88f588bf8c5f62982cd225c12
Jefferson Cunha <jwcunha@gmail.com>
2018-10-21 22:08:53 -0300

    Add order history pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,14 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
 
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
+  end
+
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
+  end
+
   # GET /orders
   # GET /orders.json
   def index
